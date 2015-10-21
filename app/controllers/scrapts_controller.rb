ce46c304f1df9a9166e26c32b8e83fa6065c313c
ColinDao <quyetdc.uet@gmail.com>
2015-10-22 04:58:04 +0700

    Search action

--- app/controllers/scrapts_controller.rb
+++ app/controllers/scrapts_controller.rb
@@ -1,4 +1,14 @@
 class ScraptsController < ApplicationController
   def home
   end
+
+  # POST /search
+  def search
+    
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+    end
+  end
+
 end
